0e07a4b0e4aa25d2076d5800be17d637e8fcd8b1
Rebeca <beca.cout@gmail.com>
2024-01-22 21:30:26 -0300

    task: complete about page

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return <header className="absolute z-30 w-full flex  items-center px-16 xl:px-6">
+    <div classname='container'>
+     <Link href={"/"} className="mt-2">
+        <Image priority={true} alt='logo cat my website' src='/logo1.svg' width={70} height={70} className="rounded-full mt-2"/>
+     </Link> 
+    </div>
+  </header>;
 };
 
 export default Header;
--- pages/about/index.js
+++ pages/about/index.js
@@ -14,6 +14,12 @@ import {
   SiAdobexd,
   SiAdobephotoshop,
 } from "react-icons/si";
+import Circles from "../../components/Circles";
+import {motion} from 'framer-motion'
+import {fadeIn} from '../../variants.js'
+import Avatar from "../../components/Avatar.js";
+import { useState } from "react";
+//import CountUp from "react-countup/build/CountUp.js";
 
 
 //  data
@@ -33,10 +39,7 @@ const aboutData = [
           <FaJava />,
         ],
       },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
+      
     ],
   },
   {
@@ -83,7 +86,69 @@ const aboutData = [
 ];
 
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0)
+  return <div className="h-full  bg-primary/30 py-32 text-center xl:text-left">
+      <Circles/>
+      <motion.div variants={fadeIn('right', 0.2)} initial="hidden" 
+      animate="show" 
+      exit="hidden" 
+      className="hidden xl:flex absolute bottom-0 -left-[370px]">
+        <Avatar/>
+
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6 
+      ">
+        <div className="flex-1  flex flex-col justify-center">
+          <h2 className="h2">Capitivation <span className="text-accent"> stories</span> birth magnificant designs.</h2>
+          
+          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+            2 Years ago, I began in Analysis and Development Systems in Instituto Federal da Bahia. 
+            Now i work with web development</p>
+        </div>
+        {/* counters */}
+        <div>
+          <div>
+            {/* experience */}
+            <div className="relative flex-1 after?w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0">
+              {/* <div> <CountUp start={0} end={2}/> +</div> */}
+              <div>Years of Experience</div>
+
+             
+            </div>
+          </div>
+        </div>
+        <div className=" flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:max-0 mb-4">
+            {aboutData.map((item, itemIndex)=> (
+              <div key={itemIndex} className={`${index === itemIndex && 'text-accent after:bg-accent after:transition-all after:duration-300'} 
+                cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] 
+               after:bg-white after:absolute after:-bottom-1 after:left-0`} 
+               onClick={()=> setIndex(itemIndex)} >
+                {item.title}
+                </div>
+            ))}
+          </div>
+
+          <div className=" py-2 xl:py-6 flex flex-col gapy-y-4 items-center xl:items-start">
+              {aboutData[index].info.map((item, itemIndex)=> (
+                <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center">
+                    <div>
+                      {item.title}
+                    </div>
+                    <div className="hidden md:flex ">-</div>
+                    <div>{item.stage}</div>
+                    {/* icons */}
+                   <div className="flex gap-x-4">
+                     {item.icons?.map((icon, iconIndex)=> (
+                      <div className="text-2xl">{icon}</div>
+                     ))}
+                   </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>;
 };
 
 export default About;
