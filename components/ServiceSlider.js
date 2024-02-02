f005051e1e209d9814f4a66d0be718805b0e124d
Rebeca Vitória de aguiar <RVD@kempetro.intranet>
2024-02-02 14:15:27 -0300

    chore: about page with slider

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,44 +1,91 @@
 // icons
 import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-} from "react-icons/rx";
-
+    RxCrop,
+    RxPencil2,
+    RxDesktop,
+    RxReader,
+    RxRocket,
+    RxArrowTopRight,
+} from 'react-icons/rx'
+import { Swiper, SwiperSlide } from 'swiper/react'
+// swiper styles
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+import { FreeMode, Pagination } from 'swiper'
 
 // data
 const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: 'Frontend',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-];
+    {
+        icon: <RxCrop />,
+        title: 'Frontend',
+        description:
+            'The construction of Landing Pages and interactive and dynamic interfaces are my differential',
+    },
+    {
+        icon: <RxPencil2 />,
+        title: 'Design',
+        description:
+            'Rely on prototype design for a basic demonstration of your project',
+    },
+    {
+        icon: <RxDesktop />,
+        title: 'Development',
+        description:
+            'Sua imaginação e críterios são contemplados com minhas habilidades em desenvolvimento',
+    },
+
+    {
+        icon: <RxRocket />,
+        title: 'SEO',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+]
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
-};
+    return (
+        <Swiper
+            breakpoints={{
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 15,
+                },
+                640: {
+                    slidesPerView: 3,
+                    spaceBetween: 15,
+                },
+            }}
+            freeMode={true}
+            pagination={{ clickable: true }}
+            modules={[FreeMode, Pagination]}
+            className='h-[240px] sm:h-[360px]'
+        >
+            {serviceData.map((service, index) => (
+                <SwiperSlide key={index}>
+                    <div
+                        className='bg-[rgba(65,47,123,0.15)] p-2 h-max rounded-lg px-6 sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(65,47,123,0.30)] 
+                    transtion-all duration-300'
+                    >
+                        {/* icon */}
+                        <div className='text-4xl text-accent mb-4'>
+                            {service.icon}
+                        </div>
+                        {/* title */}
+                        <div className='mb-8 '>
+                            <div className='text-lg mb-2'>{service.title}</div>
+                            <p className='max-w-[350px] leading-normal'>
+                                {service.description}
+                            </p>
+                        </div>
+                        {/* arrow */}
+                        <div className='text-3xl'>
+                            <RxArrowTopRight className='group hover:rotate-45 group-hover:text-accent transition-all duration-300' />
+                        </div>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    )
+}
 
-export default ServiceSlider;
+export default ServiceSlider
--- pages/about/index.js
+++ pages/about/index.js
@@ -91,34 +91,35 @@ const About = () => {
             </motion.div>
             <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
                 <div className='flex-1 flex flex-col justify-center'>
-                    <h2 className='h2'>
+                    <motion.h2
+                        variants={fadeIn('left', 0.2)}
+                        initial='hidden'
+                        animate='show'
+                        exit='hidden'
+                        className='h2'
+                    >
                         Capitivation{' '}
                         <span className='text-accent'> stories</span> birth
                         magnificant designs.
-                    </h2>
+                    </motion.h2>
 
-                    <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+                    <motion.p
+                        variants={fadeIn('left', 0.2)}
+                        initial='hidden'
+                        animate='show'
+                        exit='hidden'
+                        className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'
+                    >
                         2 Years ago, I began in Analysis and Development Systems
                         in Instituto Federal da Bahia. Now i work with web
                         development
-                    </p>
+                    </motion.p>
 
                     {/* counters */}
                     <div>
                         <div className='flex flex-1 xl:gap-x-6'>
                             {/* experience */}
                             <div className='relative flex-1'>
-                                <div className='text-2xl xl:text-4xl font font-extrabold text-accent mb-3 '>
-                                    {' '}
-                                    <CountUp start={0} end={3} duration={4} />
-                                </div>
-                                <div className=' text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
-                                    Years of Experience
-                                </div>
-                            </div>
-
-                            {/* experience */}
-                            <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 '>
                                 <div className='text-2xl xl:text-4xl font font-extrabold text-accent mb-3 '>
                                     {' '}
                                     <CountUp start={0} end={3} duration={4} />
